71ace0fe3efbb1fcc70991f6dd7204fce819c875
ahstephens6 <asthesixth@icloud.com>
2025-04-04 10:40:12 -0400

    Implemented the remaining IChessPiece classes.

--- Chess/Pieces.cs
+++ Chess/Pieces.cs
@@ -1,6 +1,7 @@
 // This file will house the piece interfaces that will be implemented later
 
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace Pieces
 {
@@ -14,17 +15,136 @@ namespace Pieces
     {
         private PieceType Type = PieceType.Pawn;
         private Color Color;
-        public Coordinate Coord;
+        public Coordinate Location;
 
         public Pawn(Color color, int x, int y)
         {
             Color = color;
-            Coord = new Coordinate(x, y);
+            Location = new Coordinate(x, y);
         }
 
         public void Move(int x, int y)
         {
-            Coord.SetCoordinate(x, y);
+            Location.SetCoordinate(x, y);
+        }
+
+        public void TakePiece(IChessPiece piece)
+        {
+
+        }
+    }
+
+    public class Knight : IChessPiece
+    {
+        private PieceType Type = PieceType.Knight;
+        private Color Color;
+        public Coordinate Location;
+
+        public Knight(Color color, int x, int y)
+        {
+            Color = color;
+            Location = new Coordinate(x, y);
+        }
+
+        public void Move(int x, int y)
+        {
+            Location.SetCoordinate(x, y);
+        }
+
+        public void TakePiece(IChessPiece piece)
+        {
+
+        }
+    }
+
+    public class Bishop : IChessPiece
+    {
+        private PieceType Type = PieceType.Bishop;
+        private Color Color;
+        public Coordinate Location;
+
+        public Bishop(Color color, int x, int y)
+        {
+            Color = color;
+            Location = new Coordinate(x, y);
+        }
+
+        public void Move(int x, int y)
+        {
+            Location.SetCoordinate(x, y);
+        }
+
+        public void TakePiece(IChessPiece piece)
+        {
+
+        }
+    }
+
+    public class Rook : IChessPiece
+    {
+        private PieceType Type = PieceType.Rook;
+        private Color Color;
+        public Coordinate Location;
+
+        public Rook(Color color, int x, int y)
+        {
+            Color = color;
+            Location = new Coordinate(x, y);
+        }
+
+        public void Move(int x, int y)
+        {
+            Location.SetCoordinate(x, y);
+        }
+
+        public void TakePiece(IChessPiece piece)
+        {
+
+        }
+    }
+
+    public class Queen : IChessPiece
+    {
+        private PieceType Type = PieceType.Queen;
+        private Color Color;
+        public Coordinate Location;
+
+        public Queen(Color color, int x, int y)
+        {
+            Color = color;
+            Location = new Coordinate(x, y);
+        }
+
+        public void Move(int x, int y)
+        {
+            Location.SetCoordinate(x, y);
+        }
+
+        public void TakePiece(IChessPiece piece)
+        {
+
+        }
+    }
+
+    public class King : IChessPiece
+    {
+        private PieceType Type = PieceType.King;
+        private Color Color;
+        public Coordinate Location;
+        private bool IsInCheck;
+        private bool IsInCheckMate;
+
+        public King(Color color, int x, int y)
+        {
+            Color = color;
+            Location = new Coordinate(x, y);
+            IsInCheck = false;
+            IsInCheckMate = false;
+        }
+
+        public void Move(int x, int y)
+        {
+            Location.SetCoordinate(x, y);
         }
 
         public void TakePiece(IChessPiece piece)
